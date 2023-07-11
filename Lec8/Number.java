11d5d94a3ab07a5d6798777a614a6f902c35bbf8
Rahul Gupta <115047275+Sciotaku@users.noreply.github.com>
2023-07-11 08:49:55 -0400

    Add files via upload

---
+++ Lec8/Number.java
@@ -0,0 +1,20 @@
+package Lec8;
+/**
+ * A constant expression
+ */
+public class Number implements Expression {
+	private double theNumber;
+	
+	public Number(double theNumber) {
+		this.theNumber = theNumber;
+	}
+	
+	public double evaluate() {
+		return theNumber;
+	}
+	
+	@Override
+	public String toString() {
+		return Double.toString(theNumber);
+	}
+}
